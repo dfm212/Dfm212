f7f6448f7142003162ee02fc6a9a00aaac6d0947
dfm212 <daniel_ve14@hotmail.com>
2015-08-07 22:22:16 -0430

    act img correction

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@
   //   };
   // });
   //
-  //
   // app.directive("articlesData", function() {
   //   return {
   //     restrict: 'E',
@@ -43,25 +42,3 @@
 
 
 })();
-
-
-
-
-  // var width = 1400;
-  // var animationSpeed = 3000;
-  // var pause = 3000;
-  // var currentSlide = 1;
-  //
-  // var $slider = $('#slider');
-  // var $slideContainer = $slider.find('.slides')
-  // var $slides = $slideContainer.find('.slide')
-  //
-  // setInterval(function() {
-  //   $slider.animate({opacity: 0}, animationSpeed, function(){
-  //     currentSlide++;
-  //     if(currentSlide === $slides.length){
-  //       currentSlide = 1;
-  //       $slideContainer.css('margin-left', 0);
-  //     }
-  //   });
-  // },pause);
